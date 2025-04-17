760064b89b88d6778f6d6858b682b9af456e2ae4
GABRIEL SCHLEMPER DANIEL <gabriel_s_daniel@estudante.sesisenai.org.br>
2025-04-16 22:19:24 -0300

    declaração de variável

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+//declaração de variável com var
+
+// var nome = "Gabriel"
+
+// var nome = "João"
+
+//declaração de variável com let
+
+// let nome = "Gabriel"
+
+// nome = "João"
+
+//variável declarada com const
+
+// const nome = "Gabriel"
+
+// nome = "João"
+
+
+// console.log(nome)
