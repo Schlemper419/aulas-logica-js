f2b81f7ae33ec07dca27710b6a32b5e89092be6c
GABRIEL SCHLEMPER DANIEL <gabriel_s_daniel@estudante.sesisenai.org.br>
2025-04-22 22:17:31 -0300

    tipos de variáveis

---
+++ exercicio01.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+        login: "Gabriel" ,
+        password:"Gabriel123",
+        cursos:["games","web"]
+    },
+    {
+        login:"Guilherme",
+        password:"Guilherme123",
+        cursos:["Design","games"]
+    }
+]
+
+console.log(`Meu login é: ${users[0].login}, minha password é: ${users[0].password}, Cursos: ${users[0].cursos[0]} e ${users[0].cursos[1]} `)
--- variaveis.js
+++ variaveis.js
@@ -12,9 +12,53 @@
 
 //variável declarada com const
 
-// const nome = "Gabriel"
+ const nome = "Gabriel" // string
 
-// nome = "João"
+ const sobrenome = "Schlemper"
 
 
-// console.log(nome)
+ const valor = 20 // number
+
+
+const islogin = false // boolean
+
+let preco //undefined
+
+
+const endereco = null //null
+
+const alunos = ["João", "Maria", "José"] //array
+
+// console.log(alunos[0]) //pega o valor do índice do array
+
+
+const alunoDoSenai = {
+    nome: "Gabriel",
+    sobrenome: "Schlemper",
+    turno: "noturno",
+    idade: 16,
+    cursos:["games", "progamação web"]
+}
+
+const alunosDoSenai = [
+    {
+        login:"João",
+        email:"joao@gmail.com",
+        cursos:["games","web"]
+    },
+    {
+        login:"Maria",
+        email:"maria@gmail.com",
+        cursos: ["design","games"]
+    }
+]
+
+//console.log(nome + " " + sobrenome)
+
+console.log(`Meu nome completo é ${nome} ${sobrenome}`)
+
+//console.log(alunoDoSenai[1].cursos[0])
+
+
+
+
