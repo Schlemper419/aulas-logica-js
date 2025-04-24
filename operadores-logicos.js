b01e5f4139b0e9059748cc3ef6ea079e2634ec87
GABRIEL SCHLEMPER DANIEL <gabriel_s_daniel@estudante.sesisenai.org.br>
2025-04-23 22:16:14 -0300

    operadores em js

---
+++ operadores-logicos.js
@@ -0,0 +1,18 @@
+// const valor01 = 50
+
+// const valor02 = 20
+
+// && => Operador de conjunção
+// || ! => Operador de disjunção
+// ! => negação
+
+// console.log(`${valor01 > 20 && valor02 >= 30 }`)
+
+const temCarteira = true
+
+const idade = 2
+
+const teste = temCarteira && idade>=18
+
+console.log(`${teste ? "Pode dirigir" : "Não pode dirigir"}`)
+
